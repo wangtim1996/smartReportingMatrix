4aa21d61579e6b89405990c93d869553dba502a6
wangtim1996 <wangtim1996@gmail.com>
2014-07-29 14:53:44 +0800

    New display function and new symbols
    Added :3 and other degrees of smiling to frowning. Some formatting
    changes so arrays don't go off the screen. Tons of if statements changed
    to switch case. slidePatternUp() updated. Cleaning up commented out
    code.

--- ledMatrixAgent.ino
+++ ledMatrixAgent.ino
@@ -155,18 +155,82 @@
     {0, 0, 0, 0, 0, 0, 0, 0} \
 }
 
+#define CATFACE8x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 1, 0, 0, 0, 1, 0, 0}, \
+    {1, 0, 0, 0, 0, 0, 1, 0}, \
+    {1, 0, 0, 1, 0, 0, 1, 0}, \
+    {1, 0, 0, 1, 0, 0, 1, 0}, \
+    {0, 1, 1, 0, 1, 1, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+
+#define SMILE08x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 1, 1, 1, 1, 0, 0}, \
+    {0, 1, 0, 0, 0, 0, 1, 0}, \
+    {1, 0, 0, 0, 0, 0, 0, 1}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+
+
+#define SMILE18x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 1, 1, 1, 1, 1, 1, 0}, \
+    {1, 0, 0, 0, 0, 0, 0, 1}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+#define SMILE28x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 1, 1, 1, 1, 1, 1, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+#define SMILE38x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {1, 0, 0, 0, 0, 0, 0, 1}, \
+    {0, 1, 1, 1, 1, 1, 1, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+#define SMILE48x8 { \
+    {0, 0, 0, 0, 0, 0, 0, 0},  \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {1, 0, 0, 0, 0, 0, 0, 1}, \
+    {0, 1, 0, 0, 0, 0, 1, 0}, \
+    {0, 0, 1, 1, 1, 1, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0}, \
+    {0, 0, 0, 0, 0, 0, 0, 0} \
+}
+
 
 byte row = 0;//********** row col change
 byte leds[8][8];
 
 // pin[xx] on led matrix connected to nn on Arduino (-1 is dummy to make array start at pos 1)
-int pins[17]= {-1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15 ,16 ,17};
+byte pins[17]= {-1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15 ,16 ,17};
 
 // col[xx] of leds = pin yy on led matrix
-int cols[8] = {pins[2], pins[8], pins[16], pins[1], pins[12], pins[15], pins[11], pins[14]};
+byte cols[8] = {pins[2], pins[8], pins[16], pins[1], pins[12], pins[15], pins[11], pins[14]};
 //
 // row[xx] of leds = pin yy on led matrix
-int rows[8] = {pins[7], pins[6], pins[4], pins[13], pins[3], pins[9], pins[10], pins[5]};
+byte rows[8] = {pins[7], pins[6], pins[4], pins[13], pins[3], pins[9], pins[10], pins[5]};
 /*
 // col[xx] of leds = pin yy on led matrix
 int cols[8] = {pins[5], pins[10], pins[9], pins[3], pins[13], pins[4], pins[6], pins[7]};
@@ -176,12 +240,48 @@ int rows[8] = {pins[2], pins[8], pins[16], pins[1], pins[11], pins[15], pins[12]
 */
 byte symbols[][8][8] = 
 {
-  SPACE8x8,SMILE8x8,FROWN8x8,EXCLAIM8x8,QUESTION8x8,FULLSTOP8x8,D8x8,E8x8,H8x8,L8x8,O8x8,R8x8,W8x8
+  SPACE8x8,
+  SMILE8x8,
+  FROWN8x8,
+  CATFACE8x8,
+  SMILE08x8,
+  SMILE18x8,
+  SMILE28x8,
+  SMILE38x8,
+  SMILE48x8,
+  EXCLAIM8x8,
+  QUESTION8x8,
+  FULLSTOP8x8,
+  D8x8,
+  E8x8,
+  H8x8,
+  L8x8,
+  O8x8,
+  R8x8,
+  W8x8
 };
 
 enum symbolLib
 {
-  SPACE,SMILE,FROWN,EXCLAIM,QUESTION,FULLSTOP,D,E,H,L,O,R,W
+  SPACE,
+  SMILE,
+  FROWN,
+  CATFACE,
+  SMILE0,
+  SMILE1,
+  SMILE2,
+  SMILE3,
+  SMILE4,
+  EXCLAIM,
+  QUESTION,
+  FULLSTOP,
+  D,
+  E,
+  H,
+  L,
+  O,
+  R,
+  W
 };
 
 
@@ -249,7 +349,8 @@ void setup() {
   Serial.println(sizeof(&patternCompendium[0]));
 }
 
-void loop() {
+void loop() 
+{
   
   while(Serial.available() > 0)
   {
@@ -259,38 +360,48 @@ void loop() {
     
     //if (Serial.read() == '/n')
     {
-      if(input == 1)
+      switch(input)
       {
-        sequence = numHelloWorld;
-        slide = true;
-
-      }
-      else
-      {
-        if(input == 4)
+        case 1:
+        {
+          sequence = numHelloWorld;
+          slide = true;
+          break;
+        }
+        case 2:
         {
           sequence = numTest;
           slide = true;
+          break;
         }
-        pattern = 0; //beginning of hello world
-        
-        
+        case 3:
+        {
+          setSymbol(SMILE);
+          Serial.println("set smile");
+          slide = false;
+          break;
+        }
+        case 4:
+        {
+          setSymbol(FROWN);
+          Serial.println("set frown");
+          slide = false;
+          break;
+        }
+        case 5:
+        {
+          setSymbol(CATFACE);
+          Serial.println("set catface");
+          slide = false;
+          break;
+        }
+        default:
+        {
+          pattern = 0;
+        }
       }
-      if(input == 2)
-      {
-        slide = false;
-        setSymbol(1);
-        Serial.println("set smile");
-      }
-      if(input == 3)
-      {
-        slide = false;
-        setSymbol(2);
-        Serial.println("set frown");
-      }
-      
-      
     }
+    
   }
   if(slide)
   {
@@ -300,7 +411,7 @@ void loop() {
     {
       pattern = 0;//beginning of hello world
     }
-    slidePatternLeft(sequence, pattern, 60);  
+    animate(sequence, pattern, 200, 100);  
   }
 }
 
@@ -339,7 +450,7 @@ void slidePatternLeft(int sequence, int pattern, int del) {
   }
 }
 
-void slidePatternUp(int pattern, int del)
+void slidePatternUp(int sequence, int pattern, int del)
 {
   for (int l = 0; l < 8; l++) //L
   {
@@ -352,25 +463,37 @@ void slidePatternUp(int pattern, int del)
     }
     for (int j = 0; j < 8; j++) 
     {
-      leds[7][j] = symbols[patternHelloWorld[pattern]][0+l][j];//L
+      leds[7][j] = symbols[patternCompendium[sequence][pattern]][0+l][j];//L
     }
     delay(del);
   }
 }  
 
+void animate(int sequence, int pattern, int del, int offDel)
+{
+  clearLeds();
+  delay(offDel);
+  for (int i = 0; i < 8; i++) 
+  {
+    for (int j = 0; j < 8; j++) 
+    {
+      leds[j][i] = symbols[patternCompendium[sequence][pattern]][j][i];
+    }
+  }
+  delay(del);
+}
+
 
 // Interrupt routine
 void display() {
-  digitalWrite(rows[row], HIGH);
-  //digitalWrite(cols[col], LOW);  // Turn whole previous column off
+  digitalWrite(rows[row], HIGH);// Turn whole previous column off
   row++;
   if (row == 8) {
     row = 0;
   }
   for (int col = 0; col < 8; col++) {
     if (leds[col][7 - row] == 1) {
-        digitalWrite(cols[col], HIGH);
-        //digitalWrite(rows[row], LOW);  // Turn on this led
+        digitalWrite(cols[col], HIGH); // Turn on this led 
     }
     else {
       digitalWrite(cols[col], LOW); // Turn off this led
@@ -378,21 +501,4 @@ void display() {
   }
   digitalWrite(rows[row], LOW); // Turn whole column on at once (for equal lighting times)
 }
-/* //old version
-void display() {
-  digitalWrite(cols[col], LOW);  // Turn whole previous column off
-  col++;
-  if (col == 8) {
-    col = 0;
-  }
-  for (int row = 0; row < 8; row++) {
-    if (leds[col][7 - row] == 1) {
-        digitalWrite(rows[row], LOW);  // Turn on this led
-    }
-    else {
-      digitalWrite(rows[row], HIGH); // Turn off this led
-    }
-  }
-  digitalWrite(cols[col], HIGH); // Turn whole column on at once (for equal lighting times)
-}
-*/
+
